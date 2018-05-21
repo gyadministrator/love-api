7bb489b61376164a44e4d235578923ca83934cbd
qgz-dream <1757282455@qq.com>
2018-05-22 01:03:14 +0800

    相册

---
+++ src/main/java/com/gy/love/loveapi/controller/AlbumController.java
@@ -0,0 +1,17 @@
+package com.gy.love.loveapi.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 作者：qiguizhen
+ * 邮箱：1757282455@qq.com
+ */
+@RestController
+@RequestMapping("/album")
+@Api(value = "心灵相册")
+public class AlbumController {
+
+
+}
