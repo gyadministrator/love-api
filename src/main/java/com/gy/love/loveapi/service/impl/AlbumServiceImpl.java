833e77cb0397a1cb1af5af66c64c465cbc0a0867
qgz-dream <1757282455@qq.com>
2018-05-22 10:20:33 +0800

    相册

---
+++ src/main/java/com/gy/love/loveapi/service/impl/AlbumServiceImpl.java
@@ -0,0 +1,13 @@
+package com.gy.love.loveapi.service.impl;
+
+import com.gy.love.loveapi.service.AlbumService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 作者：qiguizhen
+ * 邮箱：1757282455@qq.com
+ */
+@Service
+public class AlbumServiceImpl implements AlbumService {
+
+}
