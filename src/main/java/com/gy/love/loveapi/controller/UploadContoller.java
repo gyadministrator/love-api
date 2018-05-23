ea2551cee090c7551484e0b1a429f52c62fe88ce
高运 <1562211757@qq.com>
2018-05-23 15:46:58 +0800

    解决图片上传问题。

--- src/main/java/com/gy/love/loveapi/controller/UploadContoller.java
+++ src/main/java/com/gy/love/loveapi/controller/UploadContoller.java
@@ -2,11 +2,9 @@ package com.gy.love.loveapi.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.gy.love.loveapi.utils.AliyunOSSUtil;
-import com.gy.love.loveapi.utils.FileUtils;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -16,7 +14,7 @@ import java.util.Map;
 @RequestMapping("/upload")
 public class UploadContoller {
 
-    /*@ApiOperation(value = "图片上传")
+    @ApiOperation(value = "图片上传")
     @PostMapping(value="/image",produces = {"application/json;charset=utf-8"})
     public String image(MultipartFile file, HttpServletRequest request){
         Map<String,Object> map=new HashMap<>();
@@ -26,7 +24,7 @@ public class UploadContoller {
             AliyunOSSUtil aliyunOSSUtil=new AliyunOSSUtil();
             String image=aliyunOSSUtil.uploadFile(file,"image");
 
-            image="https://love-api.oss-cn-beijing.aliyuncs.com/"+image;
+            image="https://love-api.oss-cn-beijing.aliyuncs.com/image/"+image;
 
             map.put("code",200);
             map.put("image",image);
@@ -38,32 +36,5 @@ public class UploadContoller {
         String result= JSON.toJSONString(map);
 
         return result;
-    }*/
-
-    @ApiOperation(value = "图片上传")
-    @GetMapping(value = "/image", produces = {"application/json;charset=utf-8"})
-    public String image(@RequestParam("path") String path, HttpServletRequest request) {
-        Map<String, Object> map = new HashMap<>();
-        String s = FileUtils.GenerateImage(path);
-        MultipartFile file = FileUtils.createImg(s);
-        try {
-            //String path=request.getSession().getServletContext().getRealPath("\\upload\\");
-            //String image=FileUtil.uploadFile(file,path);
-            AliyunOSSUtil aliyunOSSUtil = new AliyunOSSUtil();
-            String image = aliyunOSSUtil.uploadFile(file, "image");
-
-            image = "https://love-api.oss-cn-beijing.aliyuncs.com/" + image;
-
-            map.put("code", 200);
-            map.put("image", image);
-        } catch (Exception e) {
-            map.put("code", -1);
-            e.printStackTrace();
-        }
-
-        String result = JSON.toJSONString(map);
-
-        return result;
     }
-
 }
